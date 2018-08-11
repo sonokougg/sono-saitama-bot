6f87c3f972d9e1c3f0e79bd46f9385c35d5a5607
Koki Sonoda <koki.sonoda1304@gmail.com>
2018-08-11 15:59:05 +0900

    chore: add filtering if text length less than 20

--- src/redux/middlewares/bot.js
+++ src/redux/middlewares/bot.js
@@ -4,11 +4,20 @@ const botMiddleware = store => next => (action) => {
     return;
   }
   if (!action.saitama) {
-    store.dispatch({ type: 'bot/LOADING' });
-    setTimeout(() => {
-      store.dispatch({ type: 'chatbot/SEND_TEXT', saitama: true, payload: '正義執行' });
-      store.dispatch({ type: 'bot/LOADED' });
-    }, 4000);
+    if (action.payload.length <= 20) {
+      store.dispatch({ type: 'bot/LOADING' });
+      setTimeout(() => {
+        store.dispatch({ type: 'chatbot/SEND_TEXT', saitama: true, payload: '正義執行' });
+        store.dispatch({ type: 'bot/LOADED' });
+      }, 4000);
+    }
+    if (action.payload.length > 20) {
+      store.dispatch({ type: 'bot/LOADING' });
+      setTimeout(() => {
+        store.dispatch({ type: 'chatbot/SEND_TEXT', saitama: true, payload: '話が長い！２０文字以下でまとめやがれ！' });
+        store.dispatch({ type: 'bot/LOADED' });
+      }, 4000);
+    }
   }
   next(action);
 };
